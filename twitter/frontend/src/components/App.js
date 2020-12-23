329c57cc0bc18de7f78c3081564f14a4623370e9
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-23 23:30:15 +0200

    implemented redux

--- twitter/frontend/src/components/App.js
+++ twitter/frontend/src/components/App.js
@@ -1,13 +1,29 @@
-import React, { Component } from 'react'
-import ReactDOM from 'react-dom'
+import React, { Component, Fragment } from 'react';
+import ReactDOM from 'react-dom';
+import './App.css';
+import Sidebar from './Sidebar/Sidebar';
+import Feed from './Feed/Feed';
+import Widgets from './Widgets/Widgets';
+import Header from './layout/Header';
+
+import store from '../store';
+import { Provider } from 'react-redux';
 
 class App extends Component {
-    render(){
-        return(
-            <h3>React</h3>
-        )
-    }
+  render() {
+    return (
+      <Provider store={store}>
+        <Fragment>
+          <Header />
+          <div className='app'>
+            <Sidebar />
+            <Feed />
+            <Widgets />
+          </div>
+        </Fragment>
+      </Provider>
+    );
+  }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'))
-
+ReactDOM.render(<App />, document.getElementById('app'));
