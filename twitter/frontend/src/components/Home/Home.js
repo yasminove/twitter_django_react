ec9b6f041992a4078f258043ede941853bc5eb4e
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-23 23:47:24 +0200

    implemented redux

---
+++ twitter/frontend/src/components/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'; 
+import './Home.css'
+const Home = () => {
+    return (
+        <div className="home">
+            <div className="home__header">
+                <h2>Home</h2>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
