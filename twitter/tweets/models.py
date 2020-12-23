07493e237dd90c11c3cb3f4534fdda547eca0863
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-23 23:34:39 +0200

    implemented redux

--- twitter/tweets/models.py
+++ twitter/tweets/models.py
@@ -6,5 +6,8 @@ from django.contrib.auth.models import User
 
 class Tweet(models.Model):
     content = models.TextField()
+    profile_pic = models.URLField(default='pic')
+    displayName = models.CharField(max_length=50, default='Twitter User')
+    username = models.CharField(max_length=50, default='username')
     tweeted_at = models.DateTimeField(default=timezone.now)
     # author = models.ForeignKey(User, on_delete=models.CASCADE)
