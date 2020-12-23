cab6360dae7a2a52fcd9aca867c1588cce9a73e2
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-23 23:37:23 +0200

    implemented redux

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -37,12 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'tweets', 
     'rest_framework',
     'frontend'
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
