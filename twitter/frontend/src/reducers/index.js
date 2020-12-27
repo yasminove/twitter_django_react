52248aede6920bbc14f333131326f8b0bfbf3073
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-27 21:47:24 +0200

    finished backend authentication

--- twitter/frontend/src/reducers/index.js
+++ twitter/frontend/src/reducers/index.js
@@ -1,6 +1,9 @@
 import { combineReducers } from 'redux';
 import posts from './posts';
-
+import errors from './errors';
+import messages from './messages';
 export default combineReducers({
   posts,
+  errors,
+  messages,
 });
