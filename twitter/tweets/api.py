167ac89ee45f96ed99b586db0a672b6623cd4781
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-27 21:49:23 +0200

    finished backend authentication

--- twitter/tweets/api.py
+++ twitter/tweets/api.py
@@ -3,8 +3,14 @@ from rest_framework import viewsets, permissions
 from .serializers import TweetSerializer
 
 class TweetViewSet(viewsets.ModelViewSet):
-    queryset = Tweet.objects.all()
     permission_classes = [
-        permissions.AllowAny
+        permissions.IsAuthenticated
     ]
+
     serializer_class = TweetSerializer
+
+    def get_queryset(self):
+        return self.request.user.tweets.all()
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)       
