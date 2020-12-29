f52e875e57e6737fa130a7d2097a1083a96a3d89
Yasmin Hillis <jasminehillis94@gmail.com>
2020-12-29 19:29:45 +0200

    refactor auth actions

--- twitter/twitter/settings.py
+++ twitter/twitter/settings.py
@@ -12,6 +12,12 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import dj_database_url 
+
+
+import django_heroku
+django_heroku.settings(locals())
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -26,6 +32,9 @@ SECRET_KEY = 'z2$erngt^w*nqjle+(5$t_2o3--p9*xasp-3&+c2_gg#_)j$-c'
 DEBUG = True
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS = ['twitter-clone-django-react.herokuapp.com']
+
 
 
 # Application definition
@@ -55,6 +64,7 @@ REST_FRAMEWORK = {
 
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -134,3 +144,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
+STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+
+
+# Extra lookup directories for collectstatic to find static files
+STATICFILES_DIRS = (
+    os.path.join(PROJECT_ROOT, 'static'),
+)
+
+#  Add configuration for static files storage using whitenoise
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+
+prod_db  =  dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(prod_db)
